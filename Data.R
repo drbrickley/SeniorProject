deafc88ab201d86120c6f355144fb4fe1ba69638
drbrickley <derek.r.brickley@lawrence.edu>
2022-01-16 18:08:40 -0600

    Tidying Athletics Data

--- Data.R
+++ Data.R
@@ -19,11 +19,51 @@ GSR <- GSR %>% mutate("Start Year" = `Cohort Year` + 5,
 
 GSR$`Academic Year` <- str_c(GSR$`Start Year`, "-", GSR$`End Year`)
 
-Academics <- GSR %>% inner_join(APR, by = c("School", "Sport", "State", "Academic Year")) %>%
+GSR <- GSR %>%
   select(-`Start Year`, -`End Year`, -`GSR Report`, -`FGR Report`)
 
-unique(Academics$Sport)
+Baseball <- read_xlsx("Baseball.xlsx")
 
-unique(Academics$`Academic Year`)
+Baseball <- Baseball %>% mutate(Sport = "Baseball") %>%
+  rename("Pct" = PCT)
 
-sum(is.na(Academics$`Academic Year`))
+Football <- read_xlsx("Football.xlsx")
+
+Football <- Football %>% mutate(Sport = "Football") %>%
+  rename("Pct" = Pct)
+
+Men_Basketball <- read_xlsx("Men_Basketball.xlsx")
+
+Men_Basketball <- Men_Basketball %>% mutate(Sport = "Men's Basketball") %>%
+  rename("Pct" = Pct)
+
+Men_Soccer <- read_xlsx("Men_Soccer.xlsx")
+
+Men_Soccer<- Men_Soccer %>% mutate(Sport = "Men's Soccer") %>%
+  rename("Pct" = Pct.)
+
+Women_Basketball <- read_xlsx("Women_Basketball.xlsx")
+
+Women_Basketball <- Women_Basketball %>% mutate(Sport = "Women's Basketball") %>%
+  rename("Pct" = Pct)
+
+Softball <- read_xlsx("Softball.xlsx")
+
+Softball <- Softball %>% mutate(Sport = "Softball") %>%
+  rename("Pct" = PCT)
+
+Women_Soccer <- read_xlsx("Women_Soccer.xlsx")
+
+Women_Soccer <- Women_Soccer %>% mutate(Sport = "Women's Soccer") %>%
+  rename("Pct" = Pct.)
+
+Men_IceHockey <- read_xlsx("Men_IceHockey.xlsx")
+
+Men_IceHockey <- Men_IceHockey %>% mutate(Sport = "Men's Ice Hockey") %>%
+  rename("Pct" = Pct.)
+
+Women_IceHockey <- read_xlsx("Women_IceHockey.xlsx")
+
+Women_IceHockey <- Women_IceHockey %>% mutate(Sport = "Women's Ice Hockey") %>%
+  rename("Pct" = Pct.)
+
